4cf746d809441af26fe95f1b3e437c2d70d846f6
Pyunghyuk Yoo <yoophi@gmail.com>
2016-01-11 00:29:47 +0900

    admin page

---
+++ sample/admin/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.config import Config
+from sample.models import db, Post, User, Token
+
+from sample.models import Client
+
+app = Flask(__name__)
+config = Config(app)
+admin = Admin(app)
+db.init_app(app)
+
+app.config.from_yaml(search_paths=('/etc/sample', os.path.dirname(os.path.dirname(app.root_path))))
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+admin.add_view(ModelView(Client, db.session))
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Token, db.session))
+
+if __name__ == '__main__':
+    app.run(debug=True)
