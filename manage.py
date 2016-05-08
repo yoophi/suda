14e67987e75032f764a9e4e14d57bbce48b33c20
Pyunghyuk Yoo <yoophi@gmail.com>
2016-05-08 23:24:05 +0900

    OAuth client 를 cli 환경에서 생성할 수 있도록 add_client 명령을 manage.py 에 추가

--- manage.py
+++ manage.py
@@ -2,12 +2,15 @@
 # coding: utf-8
 
 import os
-
+import click
+import shortuuid
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script.commands import ShowUrls
+from sqlalchemy.exc import IntegrityError
 
 from suda import create_app, db
+from suda.models import Client
 
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
@@ -49,6 +52,39 @@ def test(coverage=False):
         COV.html_report(directory=covdir)
         print 'HTML version: file://%s/index.html' % (covdir,)
         COV.erase()
+
+
+@manager.command
+def add_client():
+    default_client_secret = shortuuid.uuid()
+
+    name = click.prompt('name')
+    client_id = click.prompt('client_id')
+    client_secret = click.prompt('client_secret', default=default_client_secret,
+                                 hide_input=True, confirmation_prompt=True)
+    description = click.prompt('description', default='sample client')
+    is_confidential = click.prompt('is_confidential', default=False, type=bool)
+    redirect_uris = click.prompt('redirect_uri', )
+    scopes = click.prompt('scopes', )
+
+    client = Client(
+        client_id=client_id,
+        client_secret=client_secret,
+        name=name,
+        description=description,
+        is_confidential=is_confidential,
+        redirect_uris_text=redirect_uris,
+        default_scopes_text=scopes,
+    )
+
+    db.session.add(client)
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        click.echo('Error occurred while add client: <%s>' % client_id)
+        db.session.rollback()
+    finally:
+        print client
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context, use_bpython=True))
